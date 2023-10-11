2a5488f2584814057d00d41cf6322c163e2747e8
Antonín Procházka <antonin@postsharp.net>
2023-10-11 18:21:20 +0200

    Release 2024.0 initialized.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,24 +1,17 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
-using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
 using Spectre.Console.Cli;
-using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2023_4;
+using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Definitions.MetalamaDependencies.V2024_0;
 
 var product = new Product( MetalamaDependencies.MetalamaFrameworkRunTime )
 {
     Solutions = new Solution[] { new DotNetSolution( "Metalama.Framework.RunTime.sln" ) },
     PublicArtifacts = Pattern.Create( "Metalama.Framework.RunTime.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering },
-
-    // This is the first version of this repo.
-    Configurations =
-        Product.DefaultConfigurations.WithValue(
-            BuildConfiguration.Public,
-            c => c with { RequiresUpstreamCheck = false } )
+    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering }
 };
 
 var commandApp = new CommandApp();
