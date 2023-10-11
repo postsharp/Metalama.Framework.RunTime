14e4d41807bd74d75fe25513db40f35ecee2febb
Antonín Procházka <antonin@postsharp.net>
2023-10-11 18:17:48 +0200

    Upstream check removed.

--- eng/src/Program.cs
+++ eng/src/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using PostSharp.Engineering.BuildTools;
+using PostSharp.Engineering.BuildTools.Build;
 using PostSharp.Engineering.BuildTools.Build.Model;
 using PostSharp.Engineering.BuildTools.Build.Solutions;
 using PostSharp.Engineering.BuildTools.Dependencies.Definitions;
@@ -9,12 +10,15 @@ using MetalamaDependencies = PostSharp.Engineering.BuildTools.Dependencies.Defin
 
 var product = new Product( MetalamaDependencies.MetalamaFrameworkRunTime )
 {
-    Solutions = new Solution[]
-    {
-        new DotNetSolution( "Metalama.Framework.RunTime.sln" )
-    },
+    Solutions = new Solution[] { new DotNetSolution( "Metalama.Framework.RunTime.sln" ) },
     PublicArtifacts = Pattern.Create( "Metalama.Framework.RunTime.$(PackageVersion).nupkg" ),
-    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering }
+    Dependencies = new[] { DevelopmentDependencies.PostSharpEngineering },
+
+    // This is the first version of this repo.
+    Configurations =
+        Product.DefaultConfigurations.WithValue(
+            BuildConfiguration.Public,
+            c => c with { RequiresUpstreamCheck = false } )
 };
 
 var commandApp = new CommandApp();
